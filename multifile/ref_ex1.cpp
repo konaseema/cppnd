3337fb99ba211c730343d44b2b2d1e906a020c94
Vasisht A. Shastry <shastryucsb@gmail.com>
2020-08-03 20:10:06 -0700

    updated ref ex1

--- multifile/ref_ex1.cpp
+++ multifile/ref_ex1.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using std::cout;
 
+int blah(int &a) { return a++; }
+
 int main() {
-  int i = 1;
-  cout << "The value of is i: " << i << "\n"; 
+  /*int i = 1;
+  cout << "The value of is i: " << i << "\n";
   int &j = i;
   j = 2;
-  cout << "The value of is i: " << i << "\n";
+  cout << "The value of is i: " << i << "\n";*/
+  int k = 1;
+  int &m = k;
+  cout << "The value of k is: " << blah(k) << "\n";
+  cout << "The value of m is: " << m << "\n";
+  return 0;
 }
